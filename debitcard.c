524d983bea468ee426931753a494a8f165a83cf7
aprameya-1234github <42702800+aprameya-1234github@users.noreply.github.com>
2019-01-11 16:33:58 +0530

    Add files via upload

---
+++ debitcard.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+int size(long long int a);
+int main()
+{
+	long long int a;
+	int count;
+	int c[200];
+	char b[100];
+	char m[1000]="MasterCard";
+        printf("Enter the type of debit card: ");
+	fgets(b,100,stdin);
+	printf("Enter Card number: ");
+	scanf("%lld",&a);
+	count=size(a);
+	for (int i=0;i<=count;i++)
+	{
+		c[i]=a%10;
+		a=a/10;
+	}
+	if (strcmp(b,m)==0&&count==15&&c[0]==5&&(c[1]==1||c[1]==2||c[1]==3||c[1]==4))
+	{
+		printf("VALID");
+	}
+	else
+	{
+		printf("INVALID");
+	}
+	return 0;
+}
+int size(long long int a)
+{
+	int count=0;
+	while (a!=0)
+	{
+		a/=10;
+		++count;
+	}
+	return count;
+}
+
+		
+		
+
